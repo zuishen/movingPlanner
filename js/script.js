3bae42c48eb1e037e17f87f87ffbe07c3acf444e
zuishen <shenzui_da@hotmail.com>
2017-01-26 05:07:30 -0800

    add code for using nytimes api to get json data

--- js/script.js
+++ js/script.js
@@ -14,10 +14,25 @@ function loadData() {
     // load streetview
 
     // YOUR CODE GOES HERE!
-    var location = $('#street').val() + ', ' + $('#city').val();
+    var city = $('#city').val();
+    var street = $('#street').val();
+    var location = street + ', ' + city;
     $greeting.text('So, you want to live at ' + location + '?');
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+
         location+'">');
+
+    var q = "https://api.nytimes.com/svc/search/v2/articlesearch.json?" 
+        + $.param({'api-key' : "c55d633d4efb4126b56d8eee62d24a34",
+                    'q' : city
+                });
+    
+    $.getJSON(q, function(json) {
+        var items = [];
+        $.each( json.response.docs, function( i, item ) {
+            $("#nytimes-articles").append("<li class='article'><a href='"+item.web_url+"'>"+item.headline.main+"</a><p>"+item.lead_paragraph+"</p></li>");
+        });
+
+    });
     return false;
 };
 
